da77d5d1d5c62c831818181fe7204e2c3800b0e3
Yarency <yarency.lizbeth@gmail.com>
2024-12-04 16:51:06 -0800

    update

--- app/tabs/AddEntry.js
+++ app/tabs/AddEntry.js
@@ -7,7 +7,7 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export default function AddEntry() {
-
+// adding a comment for testing
   const router = useRouter();
   const [text, setText] = useState("");
 
